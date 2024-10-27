e86bcd40ea5ad4cd49bb6e5fdc819f2442891033
cloudopopop <leothomas1499@gmail.com>
2024-10-28 00:22:42 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfather
+BOT_TOKEN=("7602856365:AAHteprM1W5XN26y_xA52EpqUfH9nWZySns")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("LANKAxxCLOUD")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1648256606")
+
+
+
+
+
+  
